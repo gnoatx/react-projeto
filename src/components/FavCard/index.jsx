c23800ebfcb9dfee00e67e1a2842c4bc236ffb4c
gnoatx <85642178+gnoatx@users.noreply.github.com>
2024-09-23 19:44:50 -0300

    FavCard atualizado, JSON server instalado

--- src/components/FavCard/index.jsx
+++ src/components/FavCard/index.jsx
@@ -1,35 +1,59 @@
-import { color, exercises } from "../../assets/exercises.jsx"
-import styles from "./FavCard.module.css"
-import { Star } from "@phosphor-icons/react"
-import React, {useState} from 'react'
+// import { color, exercises } from "../../assets/exercises.jsx"
+// import styles from "./FavCard.module.css"
+// import { Star } from "@phosphor-icons/react"
+// import React, {useState} from 'react'
 
-function toggleFavorite(ex) {
-  let updatedFavorites;
+// function toggleFavorite(ex) {
+//   let updatedFavorites;
 
-  const [favorites, setFavorites] = useState([])
+//   const [favorites, setFavorites] = useState([])
 
-  if (favorites.includes(ex)) {
+//   if (favorites.includes(ex)) {
       
-      updatedFavorites = favorites.filter(fav => fav !== ex);
-  } else {
+//       updatedFavorites = favorites.filter(fav => fav !== ex);
+//   } else {
      
-      updatedFavorites = [...favorites, ex];
-  }
+//       updatedFavorites = [...favorites, ex];
+//   }
 
-  setFavorites(updatedFavorites);
+//   setFavorites(updatedFavorites);
 
  
-  localStorage.setItem(favorites, JSON.stringify(updatedFavorites));
-}
+//   localStorage.setItem(favorites, JSON.stringify(updatedFavorites));
+// }
 
-export function FavCard({exercise}) {
-  const currentExercise = exercises[exercise] || exercises.person
+import { exercises } from "../../assets/exercises.jsx"
+import styles from "./FavCard.module.css"
+import { Star } from "@phosphor-icons/react"
+import { useEffect, useState } from 'react'
+
+export function FavCard({ exercise }) {
+  const savedFavorites = JSON.parse(localStorage.getItem('favorites')) || []
+  const [favorites, setFavorites] = useState(savedFavorites)
+
+  const currentExercise = exercises[exercise] || exercises.person;
+
+  function toggleFavorite(ex) {
+    let updatedFavorites;
+
+    if (favorites.includes(ex)) {
+      updatedFavorites = favorites.splice(favorites.indexOf(ex), 1)
+    } else {
+      updatedFavorites = [...favorites, ex];
+    }
+
+    setFavorites(updatedFavorites);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+  }, [favorites])
 
   return (
     <div className={styles.card}>
       {currentExercise.icon}
       <span className={styles.exerciseName}>{currentExercise.name}</span>
-      <button onClick={toggleFavorite(currentExercise)} className={styles.add}><Star size={32} /></button>
+      <button onClick={() => toggleFavorite(currentExercise)} className={styles.add}><Star size={32} weight={favorites.includes(currentExercise) ? "fill" : "regular"}/></button>
     </div>
 )
 }
